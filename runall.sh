2f988d34c75a07d79672869a37185b2a1eefbbc5
Dane Lipscombe <dane.lipscombe@planpay.com>
2024-03-07 11:23:33 +1100

    Update

--- runall.sh
+++ runall.sh
@@ -14,6 +14,7 @@ for script in "${scripts[@]}"; do
 	python ${script}.py
 done
 
+cp abbr.yml ~/.config/espanso/match
 cp abbr.def ../qmk_firmware/keyboards/mushi/keymaps/dlip
 cp combos.dtsi ../zmk-sweep/config
 cp macros.dtsi ../zmk-sweep/config
