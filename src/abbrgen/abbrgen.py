ca0f4f4fab20fbd47e1d42cc0a903644058fef0c
Dane Lipscombe <danelipscombe@gmail.com>
2026-02-19 16:57:53 +1100

    Parallelize

--- src/abbrgen/abbrgen.py
+++ src/abbrgen/abbrgen.py
@@ -1,2 +1,38 @@
+import csv
+import logging
+import os
+from itertools import count
+from concurrent.futures import ProcessPoolExecutor
+from tqdm import tqdm
+
+from .utils import find_combinations
+
+
+def compute(line: tuple[int, tuple[str, str]]) -> str:
+    combinations = find_combinations(line[1][0])
+    return f"{line[0]}: {line[1][0].upper()} : {combinations}"
+
+
+def file_with_line_numbers(f):
+    for i, line in zip(count(1), f):
+        yield (i, line)
+
+
 def abbrgen() -> None:
-    print("Hello from abbrgen!")
+    with open("words.tsv") as file:
+        file = csv.reader(file, delimiter="\t")
+
+        alt_data = {}
+        if os.path.isfile("alt.tsv"):
+            logging.debug("loading alt.tsv")
+            with open("alt.tsv") as alt_file:
+                alt_file = csv.reader(alt_file, delimiter="\t")
+                for line in alt_file:
+                    if line:
+                        alt_data[line[0]] = line[1:]
+        lines = list(file_with_line_numbers(file))
+        with ProcessPoolExecutor() as executor:
+            results = list(
+                tqdm(executor.map(compute, lines, chunksize=10), total=len(lines))
+            )
+            print(results[0])
