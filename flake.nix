875213fe66a1b18598c9f53f10c5eda80f3ee6d4
Dane Lipscombe <danelipscombe@gmail.com>
2026-02-19 12:46:08 +1100

    Upgrate python

--- flake.nix
+++ flake.nix
@@ -1,35 +1,35 @@
 {
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-unstable";
+      url = "github:nixos/nixpkgs/c5dd43934613ae0f8ff37c59f61c507c2e8f980d";
     };
     flake-utils = {
       url = "github:numtide/flake-utils";
     };
   };
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
         pkgs = import nixpkgs {
           inherit system;
         };
-      in {
+      in
+      {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             python39
-            libmysqlclient
-            pkg-config
-            libffi
           ];
           shellHook = ''
-            if [ ! -d "./venv" ]; then
-            	python3 -m venv venv
+            if [ ! -d "./.venv" ]; then
+            	python3 -m venv .venv
             fi
-            source venv/bin/activate
+            source .venv/bin/activate
           '';
         };
       }
