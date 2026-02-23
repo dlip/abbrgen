8e4d5a4c5880bc94284bb76bfc20d5bdffa95008
Dane Lipscombe <danelipscombe@gmail.com>
2026-02-23 14:09:42 +1100

    Simplify dict

--- src/abbrgen/keyboards/standard.py
+++ src/abbrgen/keyboards/standard.py
@@ -4,32 +4,32 @@ from pydantic import BaseModel, Field
 from typing import Any
 
 # https://colemakmods.github.io/mod-dh/model.html
-effort_map_standard = [
+EFFORT_MAP_STANDARD = [
     [3, 2.5, 2.1, 2.3, 2.6, 3.4, 2.2, 2.0, 2.4, 3.0],
     [1.6, 1.3, 1.1, 1.0, 2.9, 2.9, 1.0, 1.1, 1.3, 1.6],
     [2.7, 2.4, 1.8, 2.2, 3.7, 2.2, 1.8, 2.4, 2.7, 3.3],
 ]
-effort_map_matrix = [
+EFFORT_MAP_MATRIX = [
     [3, 2.4, 2.0, 2.2, 3.2, 3.2, 2.2, 2.0, 2.4, 3],
     [1.6, 1.3, 1.1, 1.0, 2.9, 2.9, 1.0, 1.1, 1.3, 1.6],
     [3.2, 2.6, 2.3, 1.6, 3.0, 3.0, 1.6, 2.3, 2.6, 3.2],
 ]
 
-finger_maping = [
+FINGER_MAPPING = [
     [1, 2, 3, 4, 4, 5, 5, 6, 7, 8],
     [1, 2, 3, 4, 4, 5, 5, 6, 7, 8],
     [1, 2, 3, 4, 4, 5, 5, 6, 7, 8],
 ]
 
 
-hand_row_maping = [
+HAND_ROW_MAPPING = [
     ["tl", "tl", "tl", "tl", "tl", "tr", "tr", "tr", "tr", "tr"],
     ["ml", "ml", "ml", "ml", "ml", "mr", "mr", "mr", "mr", "mr"],
     ["bl", "bl", "bl", "bl", "bl", "br", "br", "br", "br", "br"],
 ]
 
 # you can ban chords that you find uncomfortable, this left hand side is mirrored
-banned_chords = [
+BANNED_CHORDS = [
     [
         [0, 0, 0, 0, 0],
         [0, 0, 0, 0, 1],
@@ -47,94 +47,64 @@ banned_chords = [
     ],
 ]
 
-
-class QwertyLayout(BaseModel):
-    name: Literal["qwerty"] = "qwerty"
-
-    def get_layout(self):
-        return [
-            ["b", "y", "o", "u", "", "", "l", "d", "v", "w"],
-            ["c", "i", "e", "a", "", "", "h", "t", "s", "n"],
-            ["g", "x", "j", "k", "", "", "r", "m", "f", "p"],
-        ]
-
-
-class ColemakLayout(BaseModel):
-    name: Literal["colemak"] = "colemak"
-
-    def get_layout(self):
-        return [
-            ["b", "y", "o", "u", "", "", "l", "d", "v", "w"],
-            ["c", "i", "e", "a", "", "", "h", "t", "s", "n"],
-            ["g", "x", "j", "k", "", "", "r", "m", "f", "p"],
-        ]
-
-
-class ColemakDhLayout(BaseModel):
-    name: Literal["colemak_dh"] = "colemak_dh"
-
-    def get_layout(self):
-        return [
-            ["b", "y", "o", "u", "", "", "l", "d", "v", "w"],
-            ["c", "i", "e", "a", "", "", "h", "t", "s", "n"],
-            ["g", "x", "j", "k", "", "", "r", "m", "f", "p"],
-        ]
-
-
-class CanaryLayout(BaseModel):
-    name: Literal["canary"] = "canary"
-
-    def get_layout(self):
-        return [
-            ["b", "y", "o", "u", "", "", "l", "d", "v", "w"],
-            ["c", "i", "e", "a", "", "", "h", "t", "s", "n"],
-            ["g", "x", "j", "k", "", "", "r", "m", "f", "p"],
-        ]
-
-
-class EngramLayout(BaseModel):
-    name: Literal["engram"] = "engram"
-
-    def get_layout(self):
-        return [
-            ["b", "y", "o", "u", "", "", "l", "d", "v", "w"],
-            ["c", "i", "e", "a", "", "", "h", "t", "s", "n"],
-            ["g", "x", "j", "k", "", "", "r", "m", "f", "p"],
-        ]
+LAYOUTS = {
+    "qwerty": [
+        ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+        ["a", "s", "d", "f", "g", "h", "j", "k", "l", ";"],
+        ["z", "x", "c", "v", "b", "n", "m", ",", ".", "/"],
+    ],
+    "colemak": [
+        ["q", "w", "f", "p", "g", "j", "l", "u", "y", ";"],
+        ["a", "r", "s", "t", "d", "h", "n", "e", "i", "o"],
+        ["z", "x", "c", "v", "b", "k", "m", ",", ".", "/"],
+    ],
+    "colemak_dh": [
+        ["q", "w", "f", "p", "b", "j", "l", "u", "y", ";"],
+        ["a", "r", "s", "t", "g", "m", "n", "e", "i", "o"],
+        ["z", "x", "c", "d", "v", "k", "h", ",", ".", "/"],
+    ],
+    "canary": [
+        ["w", "l", "y", "p", "b", "z", "f", "o", "u", "'"],
+        ["c", "r", "s", "t", "g", "m", "n", "e", "i", "a"],
+        ["q", "j", "v", "d", "k", "x", "h", ";", ",", "."],
+    ],
+    "engram": [
+        ["b", "y", "o", "u", "", "", "l", "d", "v", "w"],
+        ["c", "i", "e", "a", "", "", "h", "t", "s", "n"],
+        ["g", "x", "j", "k", "", "", "r", "m", "f", "p"],
+    ],
+}
 
 
 class StandardKeyboard(BaseModel):
     type: Literal["standard"] = "standard"
-    layout: (
-        QwertyLayout | ColemakLayout | ColemakDhLayout | CanaryLayout | EngramLayout
-    ) = Field(discriminator="name", default_factory=lambda: EngramLayout())
-
+    layout: Literal[tuple(LAYOUTS.keys())] = "engram"
     effort_map: ClassVar[dict] = {}
     layout_map: ClassVar[dict] = {}
     hand_row_map: ClassVar[dict] = {}
     banned_chords_sets: ClassVar[list] = []
 
     def model_post_init(self, context: Any) -> None:
-        layout = self.layout.get_layout()
+        layout = LAYOUTS[self.layout]
         for r in range(0, len(layout)):
             for c in range(0, len(layout[r])):
-                self.layout_map[layout[r][c]] = finger_maping[r][c]
-                self.effort_map[layout[r][c]] = effort_map_matrix[r][c]
-                self.hand_row_map[layout[r][c]] = hand_row_maping[r][c]
+                self.layout_map[layout[r][c]] = FINGER_MAPPING[r][c]
+                self.effort_map[layout[r][c]] = EFFORT_MAP_MATRIX[r][c]
+                self.hand_row_map[layout[r][c]] = HAND_ROW_MAPPING[r][c]
 
         # Add mirrored chords and padding
         mirrored = []
-        global banned_chords
+        global BANNED_CHORDS
         padding = [0, 0, 0, 0, 0]
-        for ban in banned_chords:
+        for ban in BANNED_CHORDS:
             mirror = []
             for r in range(0, len(ban)):
                 mirror.append(padding + list(reversed(ban[r])))
                 ban[r] += padding
             mirrored.append(mirror)
-        banned_chords += mirrored
+        BANNED_CHORDS += mirrored
 
-        for ban in banned_chords:
+        for ban in BANNED_CHORDS:
             s = set()
             for r in range(0, len(ban)):
                 for c in range(0, len(ban[r])):
